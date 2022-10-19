0932b4756d38b69fdbcc3654725a44c25bb58364
RAHAJASON Aroniaina Saotra <89457263+AroniainaSaotra@users.noreply.github.com>
2022-10-19 15:13:06 +0300

    initialisation

--- CubeRH/routes/web.php
+++ CubeRH/routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('ajouttache','AdminController@ajouttache');
+Route::get('ajouttache',['uses'=>'AdminController@ajouttache','as'=>'ajouttache']);
+
 Route::get('/', function () {
     return view('welcome');
 });
